6a507a5ae66bfd6496646dd3e25f8f7ab309acf0
Mitchel Onyebuchi <mitchelonyebuchi@gmail.com>
2026-03-19 10:59:13 -0700

    fix: correct CORS origins formatting by adding a missing comma

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     allow_origins=[
         "https://munchnow.vercel.app",
         "https://munchnow.site",
-        "https://www.munchnow.site"
+        "https://www.munchnow.site",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:3001",
